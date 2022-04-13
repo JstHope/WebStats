4e75f826ed6d6f046abfbcbf29e1f8ffb50cdd52
JstHope <68657498+JstHope@users.noreply.github.com>
2022-04-13 22:16:46 +0200

    changement de lib pour le serveur
    +flask

--- server.py
+++ server.py
@@ -1,36 +1,25 @@
-from aiohttp import web
-import socketio
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import SocketIO, join_room, leave_room, emit
 
-## Crée un serveur Async Socket IO 
-sio = socketio.AsyncServer()
-## Crée une nouvelle application web Aiohttp
-app = web.Application()
-# Attache notre application web a notre serveur
-sio.attach(app)
+app = Flask(__name__)
+app.debug = True
 
-# Pour retourner le index.html de manière envoyable pour le serveur
-def index(request):
-    with open('index.html') as f:
-        return web.Response(text=f.read(), content_type='text/html')
-def search(request):
-    with open('search.html') as f:
-        return web.Response(text=f.read(), content_type='text/html')
-
-## On attache nos fichier html a notre serveur
-app.router.add_get('/', index)
-app.router.add_get('/search', search)
+socketio = SocketIO(app)
 
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    return render_template('search.html')
 
 
-
-# Quand le serveur recois "send link" par socket
-@sio.on('send link')
-def use_link(sid, link):
-    print(f"[{sid}] {link}")
+@socketio.on('send link')
+def input_link(link):
+    emit('return test',"test")
 
 
-
-## Lancement
 if __name__ == '__main__':
-    web.run_app(app)
+    socketio.run(app)

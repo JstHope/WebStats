5bcfe8ed1320cd3aa86ddce477627c26ef06f98d
FondationSCP <89408459+Eliot10@users.noreply.github.com>
2022-04-13 17:31:26 +0200

    Ajout commentaires

--- scrap.py
+++ scrap.py
@@ -1,16 +1,21 @@
+# Importer les librairies utilisées
 import requests
 from bs4 import BeautifulSoup
 
-# Remplacer par un input quand tout marchera
+# Définir la page à scraper
 URL = "https://lcplanta.ch/"
+
+# Raccourcir la requête en une variable
 r = requests.get(URL)
+
+# Récupérer et parser le code source de la page
+soup = BeautifulSoup(r.content, "html5lib")
+
+# Récupérer les attributs href des éléments link sur la page
+for linkSoups in soup.findAll("link"):
+    linkSoups_HREF = linkSoups["href"]
+    print(linkSoups_HREF)
 
 # Voir output
 # soup = BeautifulSoup(r.content, "html5lib")
 # print(soup.prettify())
-
-soup = BeautifulSoup(r.content, "html5lib")
-
-for linkSoups in soup.findAll("link"):
-    linkSoups_Href = linkSoups["href"]
-    print(linkSoups_Href)

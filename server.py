d3f1b2c21e7722d81d670fe15d61399341d7cc27
JstHope <68657498+JstHope@users.noreply.github.com>
2022-04-13 17:59:15 +0200

    commentaire + redirection a search

--- server.py
+++ server.py
@@ -1,35 +1,36 @@
 from aiohttp import web
 import socketio
 
-## creates a new Async Socket IO Server
+## Crée un serveur Async Socket IO 
 sio = socketio.AsyncServer()
-## Creates a new Aiohttp Web Application
+## Crée une nouvelle application web Aiohttp
 app = web.Application()
-# Binds our Socket.IO server to our Web App
-## instance
+# Attache notre application web a notre serveur
 sio.attach(app)
 
-## we can define aiohttp endpoints just as we normally
-## would with no change
+# Pour retourner le index.html de manière envoyable pour le serveur
 def index(request):
     with open('index.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
+def search(request):
+    with open('search.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
 
-## If we wanted to create a new websocket endpoint,
-## use this decorator, passing in the name of the
-## event we wish to listen out for
+## On attache nos fichier html a notre serveur
+app.router.add_get('/', index)
+app.router.add_get('/search', search)
+
+
+
+
+
+# Quand le serveur recois "send link" par socket
 @sio.on('send link')
 def use_link(sid, link):
-    ## When we receive a new event of type
-    ## 'message' through a socket.io connection
-    ## we print the socket ID and the message
-    print("Socket ID: " , sid)
-    print(link)
+    print(f"[{sid}] {link}")
 
-## We bind our aiohttp endpoint to our app
-## router
-app.router.add_get('/', index)
 
-## We kick off our server
+
+## Lancement
 if __name__ == '__main__':
     web.run_app(app)

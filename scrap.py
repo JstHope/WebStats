224336d5626d2de57e797975fd9438fc6d093687
JstHope <luca.troger1@gmail.com>
2022-04-18 00:21:09 +0200

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -2,16 +2,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-def find_all(a_str, sub):
-    start = 0
-    while True:
-        start = a_str.find(sub, start)
-        if start == -1: return
-        yield start
-        start += len(sub) # use start += 1 to find overlapping matches
 
 # Définir la page à scraper
-URL = "https://lcplanta.ch/"
+URL = "https://wikidot.com"
 SSL = URL[:URL.find("://")]
 # Raccourcir la requête en une variable
 r = requests.get(URL)
@@ -23,10 +16,9 @@ soup = BeautifulSoup(r.content, "html5lib")
 ###
 ###     SRC
 ###
+scriptSRCList = []
 
 # Faire une liste avec les attributs SRC des éléments script
-scriptSRCList = [URL]
-
 for scriptSoups in soup.findAll("script"):
     try:
         if str(scriptSoups["src"])[0:2] == '//':
@@ -37,7 +29,40 @@ for scriptSoups in soup.findAll("script"):
     except:
         print("",end="")
 
-# Aller chercher le contenu des SRC
+# Faire une liste avec les attributs href des éléments script
+for scriptSoups in soup.findAll("link"):
+    try:
+        if str(scriptSoups["href"])[0:2] == '//':
+            scriptSRCList.append(SSL + ":",scriptSoups["href"])
+        else:
+            scriptSRCList.append(scriptSoups["href"])
+    except:
+        print("",end="")
+
+raw_lib = []
+
+# Trouve les min.js
+for link in scriptSRCList:
+    if link.find(URL) != -1 or link[0] == "/":
+        if link.find(".js?ver=") != -1:
+            lib = link.split("/")[link.count("/")]
+            raw_lib.append(lib.split(".js?ver=")[0] + " " + lib.split(".js?ver=")[1])
+            
+        elif link.find(".js") != -1:
+            lib = link.split("/")[link.count("/")]
+            raw_lib.append(lib.split(".")[0])
+    else:
+        print(link)
+""" 
+# trouve les lib importé
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub) # use start += 1 to find overlapping matches
+
 for src in scriptSRCList:
     try:
         r = requests.get(src)
@@ -53,13 +78,6 @@ for src in scriptSRCList:
 
     except:
         print("Script sans attribut SRC")
-
-
-# Récupérer les attributs href des éléments link sur la page
-#for linkSoups in soup.findAll("link"):
-#    linkSoups_HREF = linkSoups["href"]
-#    print(linkSoups_HREF)
-
-# Voir output
-# soup = BeautifulSoup(r.content, "html5lib")
-# print(soup.prettify())
+ """
+print(raw_lib)
+# go tout chercher sur ca https://www.npmjs.com/package/... ou equivalent voire si dl< nbr

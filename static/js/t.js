e06684f14d684613945ad1a99521dd1210d71475
JstHope <luca.troger1@gmail.com>
2022-05-17 12:00:31 +0200

    Update t.js

--- static/js/t.js
+++ static/js/t.js
@@ -1,23 +1,33 @@
 /// fichier de test pour les fonctions js
 function beautyname(des,name){
     var clean_name = name.toLowerCase().replaceAll(" js","")
-    var clean_des = des.toLowerCase()
-    var count = 0
-    for(let k=0;k<2;k++){
-    if(clean_des.replaceAll(" ","").indexOf(clean_name) != -1){
-            let count = 0;
-            for(let i = 0;i<des.substring(clean_des.indexOf(clean_name),clean_name.length).length;i++){
-                if (des[i] == " "){start+=1}
-            }
-            return des.substring(clean_des.replaceAll(" ","").indexOf(clean_name) + count,clean_des.indexOf(clean_name) + clean_name.length)
-        }
-        
-        else{clean_name = clean_name.split(".")[0]}
-    }
-    return name}
+    var clean_des = des.toLowerCase();
 
-des = ["Promote your business online with TrafficJunky. Reach audiences across PC, mobile, and tablet","The Google APIs Explorer is is a tool that helps you explore various Google APIs interactively."]
-name = ["trafficjunky.com","googleapis.com"]
+    for (let k =0; k<2;k++){
+    count =0;
+    start = 0;
+    for(var i = 0; count<clean_name.length && i<des.length;i++){
+        if (clean_des[i] == clean_name[count] && start ==0){
+            count+=1;
+            start = i-1;
+        }else{
+            if (clean_des[i] == clean_name[count]){
+                count+=1;
+            }
+            else{      
+               if(clean_des[i] == " "){if (start ==0){start = i+1}}
+            else{
+            count = 0;
+            start = 0;}}}
+        if (count == clean_name.length){return des.substring(start,i+1)}
+        }
+        clean_name = name.split(".")[0]
+    }
+    return name;       
+}
+
+des = ["oTrafficJunky. Reach audiences across PC, mobile, and tablet","The Google APIs Explorer is is a tool that helps you explore various Google APIs interactively."]
+name = ["trafficjunky.com","googleapis"]
 
 console.log(beautyname(des[0],name[0]))
 console.log(beautyname(des[1],name[1]))
